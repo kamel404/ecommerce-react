1f48c7ed1557fe09d036c984c8624fa6a9ed6123
Kamel Faour <10121641@mu.edu.lb>
2024-08-20 13:38:35 +0300

    Contact/About pages made

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import About from './Components/About';
+import Contact from './Components/Contact';
 
 const Home = React.lazy(() => import('./Components/Home'));
 const Products = React.lazy(() => import('./Components/Products'));
@@ -8,6 +10,8 @@ const routes = [
   { path: "/", element: <Home /> },
   { path: "/products", element: <Products /> },
   { path: "/product/:id", element: <Product /> },
+  { path: "/about", element: <About /> },
+  { path: "/contact", element: <Contact /> },
 ];
 
 export default routes;
